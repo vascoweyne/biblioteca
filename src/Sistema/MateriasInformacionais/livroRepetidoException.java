5c3aadeae36ea69ef8660a31e71925ab00480e8b
julianogueira3 <julia.mjn@edu.unifor.br>
2022-12-07 17:17:33 -0300

    sistema em produção

--- src/Sistema/MateriasInformacionais/livroRepetidoException.java
+++ src/Sistema/MateriasInformacionais/livroRepetidoException.java
@@ -1,10 +1,20 @@
 package Sistema.MateriasInformacionais;
 
 
+import java.util.Scanner;
+
 public class livroRepetidoException extends Exception {
 
     public livroRepetidoException() {
-        super("Livro ja cadastrado! Favor cadastrar outro");
+        System.out.println("Livro ja cadastrado! Favor cadastrar outro");
+    }
+    public livroRepetidoException(String message) {
+       super(message);
+    }
+    public static boolean isLivroValido()throws  livroRepetidoException{
+        System.out.println("digite outro livro");
+        String r= Scanner.nextLine();
+        for(Livro livro: livros)
     }
 
 }
